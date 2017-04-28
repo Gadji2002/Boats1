3fb5c9f51f1e87b517bc79a7c87c436c5af270da
Гаджи Кадиев < >
2017-04-28 12:27:14 +0300

    123

--- Assets/Scripts/Ship/ShipMotion.cs
+++ Assets/Scripts/Ship/ShipMotion.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MotionShip : MonoBehaviour
+public class ShipMotion : MonoBehaviour
 {
 
 
--- Assets/Scripts/Ship/ShipShooting.cs
+++ Assets/Scripts/Ship/ShipShooting.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Shooting2 : MonoBehaviour
+public class ShipShooting : MonoBehaviour
 {
 	public float delay = 1f;
 	// public int playerNumber = 1;
